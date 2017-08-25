07442db5a779618ca592675921cb389884aab506
hayyan <hayyan-ebrahem@hotmail.com>
2017-08-25 16:23:24 +0200

    some fixes

--- sales_adv_analysis/report/sales_adv_report.py
+++ sales_adv_analysis/report/sales_adv_report.py
@@ -1,4 +1,39 @@
-from odoo import fields, models
+# -*- coding: utf-8 -*-
 
-class SalesAdvReport(models.AbstractModel):
-    _name = 'report.sales_adv_analysis.report_sale_adv'
+from odoo import models, fields, api
+
+
+class SalesAdvAnalysis(models.AbstractModel):
+    _name = 'report.sales_adv_analysis.salesadv_analysis'
+    _inherit = 'sale.order'
+    print(' SALE ADV')
+    
+    def _get_dataframe(self, report):
+    	print('self:',self)
+    	lines = self.with_context(data['form'].get('used_context'))._get_accounts(accounts, display_account)
+    	df = pd.DataFrame(lines,columns=['sale','advertising'])
+    	return df
+    	
+
+    def _compute_current_ratio(self,report):
+    	return ' CURRENT RATIO'
+
+    @api.model
+    def render_html(self, docids, data=None):
+        self.model = self.env.context.get('active_model')
+        docs = self.env[self.model].browse(self.env.context.get('active_id'))
+        report_lines = self.with_context(data['form'].get('used_context'))._get_accounts(accounts, display_account)
+        df = pd.DataFrame(report_lines)
+        print('yessssssssssssssssssssssssssssssssssssssssssssssssssssssss\n\n')
+        # print('report_lines ::', report_lines)
+        # print('\n\n type(report_lines):',type(report_lines))
+
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': self.model,
+            'data': data['form'],
+            'docs': docs,
+            'time': time,
+            'get_account_lines': report_lines,
+        }
+        return self.env['report'].render('balance_sheet_analysis.balance_analysis', docargs)
