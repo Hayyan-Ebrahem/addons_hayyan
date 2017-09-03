5b148796cd5c6c5eb7a20a324fe7bee274b5c285
hayyan <hayyan-ebrahem@hotmail.com>
2017-09-03 10:55:46 +0200

    some fixes

--- balance_sheet_analysis/report/balance_sheet_analysis.py
+++ balance_sheet_analysis/report/balance_sheet_analysis.py
@@ -3,29 +3,32 @@
 from odoo import models, fields, api
 
 
-class BalanceAnalysis(models.AbstractModel):
+class BalanceSheetAnalysisReport(models.AbstractModel):
     _name = 'report.balance_sheet_analysis.report_sheetanalysis'
-    #_inherit = 'report.account.report_financial'
+    _inherit = 'report.account.report_financial'
     print(' BALANCE_SHEET')
-    
-    def _get_dataframe(self, report):
-    	lines = self.with_context(data['form'].get('used_context'))._get_accounts(accounts, display_account)
-        print('YESSSSSSSSSSSSSSYESSSSSSSSSSSSSSSSSSSSSSSSYESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS')
-    	df = pd.DataFrame(lines,columns=['name'])
-    	return df
-    	
 
-    def _compute_current_ratio(self,report):
+
+    def _compute_report_sheetnalysis(self,report):
     	return ' CURRENT RATIO'
 
     @api.model
     def render_html(self, docids, data=None):
+        if not data.get('form') or not self.env.context.get('active_model') or not self.env.context.get('active_id'):
+            raise UserError(_("Form content is missing, this report cannot be printed."))
         self.model = self.env.context.get('active_model')
         docs = self.env[self.model].browse(self.env.context.get('active_id'))
-        report_lines = self.with_context(data['form'].get('used_context'))._get_accounts(accounts, display_account)
-        df = pd.DataFrame(report_lines)
-        print('yessssssssssssssssssssssssssssssssssssssssssssssssssssssss\n\n')
-        # print('report_lines ::', report_lines)
+        print('\n\n')
+        print('############### data:',data)
+        print('\n\n')
+        accounts =  self.env['account.financial.report'].browse([5,6])
+        data['form']['account_report_id']=accounts#.update('account_report_id',4)
+
+        print('\n\n')
+        print('########### accounts :',accounts)
+        #account_res = super(BalanceSheetAnalysisReport,self).get_account_lines(data)
+        report_lines = self.get_account_lines(data.get('form'))
+        print('report_lines ::', report_lines)
         # print('\n\n type(report_lines):',type(report_lines))
 
         docargs = {
@@ -36,4 +39,4 @@ class BalanceAnalysis(models.AbstractModel):
             'time': time,
             'get_account_lines': report_lines,
         }
-        return self.env['report'].render('balance_sheet_analysis.balance_analysis', docargs)
+        return self.env['report'].render('balance_sheet_analysis.report_sheetanalysis', docargs)
--- balance_sheet_analysis/wizard/report_balance_sheet_analysis.py
+++ balance_sheet_analysis/wizard/report_balance_sheet_analysis.py
@@ -7,6 +7,16 @@ class BalanceSheetAnalysisReport(models.TransientModel):
     _name = 'balance.sheet.analysis.report'
     _description = 'Balance Sheet Analysis Report'
 
+    @api.model
+    def _get_account_report(self):
+        reports = []
+        if self._context.get('active_id'):
+            menu = self.env['ir.ui.menu'].browse(self._context.get('active_id')).name
+            print('-------------- menu:',menu)
+            reports = self.env['account.financial.report'].search([('name', 'ilike', menu)])
+        return reports and reports[0] or False
+
+
     date_from = fields.Datetime(string='Start Date')
     date_to = fields.Datetime(string='End Date')
 
@@ -19,7 +29,6 @@ class BalanceSheetAnalysisReport(models.TransientModel):
         return self._print_report(data)
 
     def _print_report(self, data):
-    	data['form'].update(self.read(['date_from', 'date_to'])[0])
-        print('+++++++++++_______-----------',data,' self :',self)
+        print('+++++++++++_______----------- data :',data)
      
         return self.env['report'].get_action(self, 'balance_sheet_analysis.report_sheetanalysis', data=data)
